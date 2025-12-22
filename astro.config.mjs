bfd20978a65324d84e1c2bce02f050ce8a7c05f5
Andrii Stavskyi <an.stawski@outlook.com>
2025-12-22 23:01:15 +0200

    build: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
